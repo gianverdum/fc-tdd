16dea12ad595d1d5e98b41ec6fc74e8dac5a6bde
gianverdum <gian.verdum@gmail.com>
2025-07-18 16:18:50 -0300

    Triangulando teste e salvando novo registro

--- src/application/user_service.test.ts
+++ src/application/user_service.test.ts
@@ -1,5 +1,6 @@
 import { UserService } from "./user_service";
 import { FakeUserRepository } from "../infrastructure/repositories/fake_user_repository";
+import { User } from "../domain/entities/user";
 describe('User service unit tests', () => {
     let userService: UserService;
     let fakeUserRepository: FakeUserRepository;
@@ -9,14 +10,26 @@ describe('User service unit tests', () => {
         userService = new UserService(fakeUserRepository);
     });
 
-    it('shouuld return null when an user id invalid is used', async () => {
+    it('should return null when an user id invalid is used', async () => {
         const user = await userService.findUserById('999');
         expect(user).toBeNull();
     });
-    it('shouuld return an user when an user id valid is used', async () => {
+    it('should return an user when an user id valid is used', async () => {
         const user = await userService.findUserById('1');
         expect(user).not.toBeNull();
         expect(user?.getId()).toBe('1');
         expect(user?.getName()).toBe('Jhon Doe');
+    });
+    it('should save a new user using fake repository and return the data', async () => {
+        //Arrange
+        const newUser = new User('3', 'Alice Dantas');
+
+        await fakeUserRepository.save(newUser);
+        // Act
+        const user = await userService.findUserById('3');
+        // Assert
+        expect(user).not.toBeNull();
+        expect(user?.getId()).toBe('3');
+        expect(user?.getName()).toBe('Alice Dantas');
     });
 });
