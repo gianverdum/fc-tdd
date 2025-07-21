17f4df0ed976e0bf6f8cbdb789a2e7959118a261
gianverdum <gian.verdum@gmail.com>
2025-07-21 20:15:12 -0300

    Aprofundando o teste até o dominio

--- src/application/services/booking_service.test.ts
+++ src/application/services/booking_service.test.ts
@@ -107,4 +107,38 @@ describe('BookingService', () => {
 
         await expect(bookingService.createBooking(bookingDTO)).rejects.toThrow('User not found');
     });
+    it('should throw an error while trying booking in a date range already booked', async () => {
+        const mockProperty = {
+            getId: jest.fn().mockReturnValue('1'),
+            isAvailable: jest.fn().mockReturnValue(true),
+            validateGuestCount: jest.fn(),
+            calculateTotalPrice: jest.fn().mockReturnValue(500),
+            addBooking: jest.fn(),
+        } as any;
+
+        const mockUser = {
+            getId: jest.fn().mockReturnValue('1'),
+        } as any;
+
+        mockPropertyService.findPropertyById.mockResolvedValue(mockProperty);
+        mockUserService.findUserById.mockResolvedValue(mockUser);
+
+        const bookingDTO: CreateBookingDTO = {
+            propertyId: '1',
+            guestId: '1',
+            startDate: new Date('2024-12-20'),
+            endDate: new Date('2024-12-25'),
+            guestCount: 2,
+        };
+
+        const result = await bookingService.createBooking(bookingDTO);
+
+        mockProperty.isAvailable.mockReturnValue(false);
+        mockProperty.addBooking.mockImplementationOnce(() => {
+            throw new Error('The property is unavailable in the date range requested');
+        });
+
+        await expect(bookingService.createBooking(bookingDTO)).rejects
+            .toThrow('The property is unavailable in the date range requested');
+    });
 });
