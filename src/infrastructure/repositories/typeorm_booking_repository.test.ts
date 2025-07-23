53bffa543b671b15ef4974d773562334f0a019b4
gianverdum <gian.verdum@gmail.com>
2025-07-23 20:46:26 -0300

    Finalizado testes de BookingRepository

--- src/infrastructure/repositories/typeorm_booking_repository.test.ts
+++ src/infrastructure/repositories/typeorm_booking_repository.test.ts
@@ -71,4 +71,50 @@ describe('TypeORMBookingRepository', () => {
         expect(savedBooking?.getProperty().getId()).toBe('1');
         expect(savedBooking?.getGuest().getId()).toBe('1');
     });
+    it('should return null when booking not found', async () => {
+        const booking = await bookingRepository.findById('non-existing-id');
+        expect(booking).toBeNull();
+    });
+    it('should save a booking and then canceling', async () => {
+        const propertyRepository = dataSource.getRepository(PropertyEntity);
+        const userRepository = dataSource.getRepository(UserEntity);
+        const propertyEntity = propertyRepository.create({
+            id: '1',
+            name: 'Casa na praia',
+            description: 'Uma linda casa na praia com vista para o mar.',
+            maxGuests: 6,
+            basePricePerNight: 200,
+        });
+        await propertyRepository.save(propertyEntity);
+
+        const property = new Property(
+            '1',
+            'Casa na praia',
+            'Uma linda casa na praia com vista para o mar.',
+            6,
+            200
+        );
+
+        const userEntity = userRepository.create({
+            id: '1',
+            name: 'João Silva',
+        });
+        await userRepository.save(userEntity);
+
+        const user = new User('1', 'João Silva');
+        const dateRange = new DateRange(
+            new Date('2024-12-20'),
+            new Date('2024-12-25')
+        );
+
+        const booking = new Booking('1', property, user, dateRange, 4);
+        await bookingRepository.save(booking);
+
+        booking.cancel(new Date('2024-12-15'));
+        await bookingRepository.save(booking);
+
+        const updatedBooking = await bookingRepository.findById('1');
+        expect(updatedBooking).not.toBeNull();
+        expect(updatedBooking?.getStatus()).toBe('CANCELLED');
+    });
 });
